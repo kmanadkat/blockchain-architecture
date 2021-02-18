54962775ce1a9badf751e6e5b8cea8a66c8bec69
Krupesh Anadkat <kmanadkat@gmail.com>
2021-02-18 23:05:26 +0530

    Completed Data & App Contract Separation Lesson

--- contracts/ExerciseC6A.sol
+++ contracts/ExerciseC6A.sol
@@ -1,18 +1,21 @@
 pragma solidity ^0.4.25;
 
 contract ExerciseC6A {
+
     /********************************************************************************************/
     /*                                       DATA VARIABLES                                     */
     /********************************************************************************************/
-    
+
+    uint constant M = 2;
     struct UserProfile {
         bool isRegistered;
         bool isAdmin;
     }
 
-    address private contractOwner; // Account used to deploy contract
-    mapping(address => UserProfile) userProfiles; // Mapping for storing user profiles
-    bool public isOperational;
+    address private contractOwner;                  // Account used to deploy contract
+    mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
+
+    bool private operational = true;                                    // Blocks all state changes throughout the contract if false
 
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
@@ -21,13 +24,15 @@ contract ExerciseC6A {
     // No events
 
     /**
-     * @dev Constructor
-     *      The deploying account becomes contractOwner
-     *      The contract is set to operational
-     */
-    constructor() public {
+    * @dev Constructor
+    *      The deploying account becomes contractOwner
+    */
+    constructor
+                                (
+                                ) 
+                                public 
+    {
         contractOwner = msg.sender;
-        isOperational = true;
     }
 
     /********************************************************************************************/
@@ -38,19 +43,23 @@ contract ExerciseC6A {
     // before a function is allowed to be executed.
 
     /**
-     * @dev Modifier that requires the "ContractOwner" account to be the function caller
-     */
-    modifier requireContractOwner() {
+    * @dev Modifier that requires the "ContractOwner" account to be the function caller
+    */
+    modifier requireContractOwner()
+    {
         require(msg.sender == contractOwner, "Caller is not contract owner");
         _;
     }
 
     /**
-     * @dev Modifier that requires the contract to be operational for function to execute
-     */
-    modifier requireIsOperational() {
-        require(isOperational, "Contract is not operational");
-        _;
+    * @dev Modifier that requires the "operational" boolean variable to be "true"
+    *      This is used on all state changing functions to pause the contract in 
+    *      the event there is an issue that needs to be fixed
+    */
+    modifier requireIsOperational() 
+    {
+        require(operational, "Contract is currently not operational");
+        _;  // All modifiers require an "_" which indicates where the function body will be added
     }
 
 
@@ -58,43 +67,70 @@ contract ExerciseC6A {
     /*                                       UTILITY FUNCTIONS                                  */
     /********************************************************************************************/
 
-    /**
-     * @dev Check if a user is registered
-     *
-     * @return A bool that indicates if the user is registered
-     */
-
-    function isUserRegistered(address account) external view returns (bool) {
+   /**
+    * @dev Check if a user is registered
+    *
+    * @return A bool that indicates if the user is registered
+    */   
+    function isUserRegistered
+                            (
+                                address account
+                            )
+                            external
+                            view
+                            returns(bool)
+    {
         require(account != address(0), "'account' must be a valid address.");
         return userProfiles[account].isRegistered;
+    }
+
+    /**
+    * @dev Get operating status of contract
+    *
+    * @return A bool that is the current operating status
+    */      
+    function isOperational() 
+                            public 
+                            view 
+                            returns(bool) 
+    {
+        return operational;
     }
 
     /********************************************************************************************/
     /*                                     SMART CONTRACT FUNCTIONS                             */
     /********************************************************************************************/
 
-    function registerUser(address account, bool isAdmin)
-        external
-        requireContractOwner
-        requireIsOperational
+    function registerUser
+                                (
+                                    address account,
+                                    bool isAdmin
+                                )
+                                external
+                                requireIsOperational
+                                requireContractOwner
     {
-        require(
-            !userProfiles[account].isRegistered,
-            "User is already registered."
-        );
+        require(!userProfiles[account].isRegistered, "User is already registered.");
 
         userProfiles[account] = UserProfile({
-            isRegistered: true,
-            isAdmin: isAdmin
-        });
+                                                isRegistered: true,
+                                                isAdmin: isAdmin
+                                            });
+    }
+        /**
+    * @dev Sets contract operations on/off
+    *
+    * When operational mode is disabled, all write transactions except for this one will fail
+    */    
+    function setOperatingStatus
+                            (
+                                bool mode
+                            ) 
+                            external
+                            requireContractOwner
+    {
+            operational = mode;      
     }
 
-    function pauseContract() public requireContractOwner requireIsOperational {
-        isOperational = false;
-    }
-
-    function resumeContract() public requireContractOwner {
-        require(!isOperational, "Contract is already active");
-        isOperational = true;
-    }
 }
+
